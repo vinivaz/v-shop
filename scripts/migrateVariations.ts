ea11ed1dcad7201568194f188f37875c29d370ce
Vini <viniciusvaz.tech@gmail.com>
2025-06-21 23:42:17 -0300

    middleware finished, mobile navbar adaptation done

--- scripts/migrateVariations.ts
+++ scripts/migrateVariations.ts
@@ -1,39 +1,39 @@
-import { prisma } from "../src/lib/prisma";
+// import { prisma } from "../src/lib/prisma";
 
-async function migrateVariations() {
-  const products = await prisma.product.findMany({
-    include: { variations: true }
-  });
+// async function migrateVariations() {
+//   const products = await prisma.product.findMany({
+//     include: { variations: true }
+//   });
 
-  for (const product of products) {
-    const { id, price, stock, mainImage, additionalImages } = product;
+//   for (const product of products) {
+//     const { id, price, stock, mainImage, additionalImages } = product;
 
 
-    await prisma.variation.updateMany({
-      where: { productId: id },
-      data: { main: false },
-    });
+//     await prisma.variation.updateMany({
+//       where: { productId: id },
+//       data: { main: false },
+//     });
 
-    await prisma.variation.create({
-      data: {
-        productId: id,
-        name: "black",
-        price,
-        stock,
-        main: true,
-        images: [mainImage!, ...additionalImages],
-      },
-    });
+//     await prisma.variation.create({
+//       data: {
+//         productId: id,
+//         name: "black",
+//         price,
+//         stock,
+//         main: true,
+//         images: [mainImage!, ...additionalImages],
+//       },
+//     });
 
-    console.log(`Atualizado produto: ${product.name}`);
-  }
+//     console.log(`Atualizado produto: ${product.name}`);
+//   }
 
-  console.log("Migração concluída!");
-}
+//   console.log("Migração concluída!");
+// }
 
-migrateVariations()
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  })
-  .finally(() => prisma.$disconnect());
+// migrateVariations()
+//   .catch((err) => {
+//     console.error(err);
+//     process.exit(1);
+//   })
+//   .finally(() => prisma.$disconnect());
