3d991f8513e9b8ee65206fda74a1b312bcefbbb8
Vini <viniciusvaz.tech@gmail.com>
2025-06-23 00:45:20 -0300

    nav bar sign in sign up user button added

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,7 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/auth/authOptions";
 
-export function Header() {
+
+export async function Header() {
+
+  const session = await getServerSession(authOptions);
+
   return(
     <nav className=" z-9 w-full h-[var(--navbar-height)] fixed top-0 max-md:bottom-0 max-md:top-auto flex shrink-0 justify-center items-center bg-white">
       <div className="max-w-[1120px] w-full flex justify-between">
@@ -19,6 +25,7 @@ export function Header() {
         <div 
           className="flex flex-row mr-10 max-md:mr-0 max-md:w-full max-md:justify-evenly"
         >
+
           <Link
             href='/'
             className="mx-2 flex items-center max-md:flex-col min-md:hidden"
@@ -83,6 +90,45 @@ export function Header() {
               Cart
             </span>
           </Link>
+
+          {!session?
+          (
+            <>
+              <Link
+                href="/sign-in"
+                className="mx-2 flex items-center font-medium"
+              >
+                Entrar
+              </Link>
+              <Link
+                href="/sign-up"
+                className=" flex items-center font-medium bg-darker text-white mx-2 px-3 rounded-xl"
+              >
+                Cadastrar
+              </Link>
+            </>
+          )
+          :
+          (
+            <div
+  
+              className="mx-2 flex items-center max-md:flex-col"
+            >
+              <Image 
+                src="/icons/user-icon.svg" 
+                alt="user glass icon"
+                width={20}
+                height={20}
+              />
+              <span
+                className="ml-1 text-sm text-darker"
+              >
+                User
+              </span>
+            </div>
+          )
+        }
+
         </div>
       </div>
     </nav>
