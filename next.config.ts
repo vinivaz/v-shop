051fc4b8cd24a06ecd47a7e74174863468e9c886
Vini <viniciusvaz.tech@gmail.com>
2025-07-29 15:30:26 -0300

    configuring dev zone

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
