804f17de6cd9371b4fcf0e59d36f8300a6340469
Vini <viniciusvaz.tech@gmail.com>
2025-08-03 22:03:48 -0300

    deleting old unused files

--- src/app/api/products/search/route.ts
+++ src/app/api/products/search/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
-import { prisma } from '@/lib/prisma'; // ajuste esse path conforme seu projeto
-import { error } from 'console';
+import { prisma } from '@/lib/prisma';
+
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
